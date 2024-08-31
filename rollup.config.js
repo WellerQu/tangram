50df35bf333fbfa9e994d5c8b457adf2c3b7d7bd
nix <nix@mecx.com>
2024-08-31 12:30:38 +0800

    chore: specify the version of node runtime

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,10 @@ import typescript from '@rollup/plugin-typescript'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 
+import pkg from './package.json' with { type: 'json' }
+
+const dependencies = Object.keys(pkg.dependencies)
+
 /** @type {import('rollup').RollupOptions} */
 export default {
   input: 'src/index.ts',
@@ -12,7 +16,7 @@ export default {
     sourcemap: true,
   },
   plugins: [
-    nodeResolve({ resolveOnly: ['classnames'] }),
+    nodeResolve({ resolveOnly: dependencies }),
     commonjs(),
     typescript(),
   ],
