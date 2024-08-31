716a67c3f243ff78097a80621082aba12594d73d
nix <nix@mecx.com>
2024-08-31 12:16:53 +0800

    chore: adjust bundle configurations

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,4 @@
+/** @type {import('postcss').Processor} */
 export default {
   plugins: {
     tailwindcss: {},
--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
 import typescript from '@rollup/plugin-typescript'
-// import { nodeResolve } from '@rollup/plugin-node-resolve'
-// import commonjs from '@rollup/plugin-commonjs'
-
-import css from 'rollup-plugin-css-only'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 
 /** @type {import('rollup').RollupOptions} */
 export default {
@@ -13,9 +11,9 @@ export default {
     assetFileNames: '[name][extname]',
     sourcemap: true,
   },
-  external: ['react', 'react/jsx-runtime'],
   plugins: [
-    css({ output: 'tangram.css' }),
+    nodeResolve({ resolveOnly: ['classnames'] }),
+    commonjs(),
     typescript(),
   ],
 }
